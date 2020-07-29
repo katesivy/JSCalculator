51d069257465adf71ef7773c5f78b77b7dcd1b00
katesivy <58646769+katesivy@users.noreply.github.com>
2020-07-29 17:20:02 -0400

    added styling and alerts

--- app.js
+++ app.js
@@ -12,15 +12,19 @@ function init() {
     container.classList.add('container');
     container.style.maxWidth = '600px';
     container.style.margin = 'auto';
+    container.style.padding = '70px';
+    container.style.border = 'black 1px solid';
+    container.style.backgroundColor = 'grey';
     document.body.appendChild(container);
 
     const output = document.createElement('input');
     output.setAttribute('type', 'text');
     output.classList.add('output');
     output.style.width = '100%';
-    output.style.lineHeight = '50px';
+    output.style.lineHeight = '70px';
     output.style.fontSize = '3em';
     output.style.textAlign = 'right';
+    output.style.border = 'black 1px solid';
     container.appendChild(output);
 
     const main = document.createElement('div');
@@ -31,7 +35,7 @@ function init() {
         // console.log(value);
         btnMaker(value, addOutput);
     })
-    btnMaker('=', equalslOutput);
+    btnMaker('=', equalsOutput);
     btnMaker('C', clearOutput);
 
     function colorOutput(v) {
@@ -39,15 +43,17 @@ function init() {
         output.style.color = v;
     }
 
-    function equalslOutput() {
+    function equalsOutput() {
         console.log('=');
         colorOutput('black');
         if (output.value === "") {
             colorOutput('red');
+            alert('Please enter a valid number.');
         } else if (equals) {
             colorOutput('red');
+            alert("Invalid submission");
         } else {
-            output.value = equalsl(output.value);
+            output.value = eval(output.value);
         }
         decimal = output.value.includes('.');
     }
@@ -61,10 +67,12 @@ function init() {
     function btnMaker(txt, myFunction) {
         let btn = document.createElement('button');
         btn.setAttribute('type', 'button');
-        btn.style.width = '23%';
-        btn.style.lineHeight = '50px';
-        btn.style.margin = '1%';
+        btn.style.width = '20%';
+        btn.style.lineHeight = '70px';
+        btn.style.margin = '2%';
         btn.style.fontSize = '2em';
+        btn.style.border = 'black 1px solid';
+        btn.style.backgroundColor = 'silver';
         btn.value = txt;
         btn.textContent = txt;
         btn.addEventListener('click', myFunction);
@@ -81,6 +89,7 @@ function init() {
             if (decimal) {
                 char = '';
                 colorOutput('red')
+                alert('Decimal not valid');
             } else {
                 decimal = true;
             }
